9803e3fef2c524a013841b024550f90a3d7b4a88
TorHugo <arruda.victorhugo@outlook.com>
2023-09-23 03:14:28 -0300

    FEAT: Initial commit.

---
+++ src/main/java/com/dev/torhugo/hub_payments/HubPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.torhugo.hub_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/torhugo/hub_payments/HubPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.torhugo.hub_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
